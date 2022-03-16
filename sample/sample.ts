199a55780c3c4c91532aa847a7d256472a76c990
Rubens Pinheiro <rubens@atiz.com.br>
2022-03-16 14:45:18 -0300

    Update sample.ts

--- sample/sample.ts
+++ sample/sample.ts
@@ -4,8 +4,6 @@ const teste = async () => {
     const odac = await OdacConnection.open({ user: 'xxx', password: 'xxx', connectString: '10.0.1.10/WINT' });
     let result = await odac.getNewQuery().sql({ command: 'SELECT CODPROD FROM PCPRODUT WHERE ROWNUM < 50', bindParameters: {} });
     result === undefined ? console.log('no data found') : result.forEach(element => { console.log(element) });
-    result = await odac.getNewQuery().sql({ command: 'SELECT CODCLI FROM PCCLIENT WHERE ROWNUM < 50', bindParameters: {} });
-    result === undefined ? console.log('no data found') : result.forEach(element => { console.log(element) });
     await odac.close();
 }
 
