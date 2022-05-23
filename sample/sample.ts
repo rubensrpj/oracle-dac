f77967b2617d9bd6ab6cc9b680a8b6cc6f657e9d
Rubens Pinheiro <rubens@atiz.com.br>
2022-05-23 15:56:08 -0300

    Bug Fix NextVal Method

--- sample/sample.ts
+++ sample/sample.ts
@@ -1,10 +1,18 @@
 import { OdacConnection } from '../lib';
 
-const teste = async () => {
+const sql = async () => {
     const odac = await OdacConnection.open({ user: 'x', password: 'x', connectString: '10.0.1.10/WINT' });
     let result = await odac.getNewQuery().sql({ command: 'SELECT CODPROD FROM PCPRODUT WHERE ROWNUM < :LINHAS AND CODPROD = :CODPROD ', bindParameters: { "LINHAS": 50, "CODPROD" : 0} });
     result === undefined ? console.log('no data found') : result.forEach(element => { console.log(element) });
     await odac.close();
 }
 
-teste();
+const nextval = async () => {
+    const odac = await OdacConnection.open({ user: 'x', password: 'x', connectString: '10.0.1.10/WINT' });
+    let result = await odac.getNewQuery().nextVal('USUARIOPORTAL_SEQ');
+    console.log(result)
+    await odac.close();
+}
+
+//sql();
+nextval();
